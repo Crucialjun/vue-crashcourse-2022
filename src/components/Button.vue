b68d49e36ac491b102f78c54a7133f9f462d6860
nicho <nich.otieno@gmail.com>
2022-12-09 18:04:12 +0300

    update

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="onClick" class="btn" :style="{background : color}">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name : "ButtonComponent",
+
+    props : {
+        text :String,
+        color : String
+    },
+
+    methods: {
+        onClick(){
+            console.log('Clicked')
+        }
+    },
+
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>
+            {{title}}
+        </h1>
+        <ButtonVue text="Add Task" color="green"></ButtonVue>
+    
+    </header> 
+</template>
+
+<script>
+import ButtonVue from './Button.vue';
+    export default {
+        name: 'HeaderComponent',
+        props : {
+            "title" : String
+        },
+        components : {
+            ButtonVue
+        }
+    }
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
